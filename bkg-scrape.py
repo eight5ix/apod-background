2e2c3d4b2ce1593d6746a2ff5bfbc67f393dd488
eight5ix <32492627+eight5ix@users.noreply.github.com>
2022-07-06 09:19:26 -0400

    Update bkg-scrape.py

--- bkg-scrape.py
+++ bkg-scrape.py
@@ -2,19 +2,25 @@ import requests
 from bs4 import BeautifulSoup 
 import ctypes
 
+# Make a request to the url formula
 def getdata(url): 
     r = requests.get(url) 
     return r.text 
-    
+
+# Identify image url
 htmldata = getdata("https://apod.nasa.gov/apod/astropix.html") 
+# Parse image
 soup = BeautifulSoup(htmldata, 'html.parser') 
+# When the image is found, print the url
 for item in soup.find_all('img'):
     url = "https://apod.nasa.gov/apod/"+item['src']
     print(url)
-    
+
+# Move image to local
 img_data = requests.get(url).content
-with open('C:/Users/tkaplan/Documents/conda/ScrapeAPOD/astropix.jpg', 'wb') as handler:
+with open('PATH/astropix.jpg', 'wb') as handler:
     handler.write(img_data)
-    
+
+# Change the background   
 SPI_SETDESKWALLPAPER = 20
-ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, 'C:/Users/tkaplan/Documents/conda/ScrapeAPOD/astropix.jpg' , 0)
+ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, 'PATH/astropix.jpg' , 0)
